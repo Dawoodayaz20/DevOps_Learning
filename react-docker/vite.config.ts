8e0015b48a4bd91c1699ccf457a8bc8121461613
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-01-07 01:42:55 +0500

    Edit vite and App files.

--- react-docker/vite.config.ts
+++ react-docker/vite.config.ts
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Enable polling for file changes
+    },
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,
+    strictPort: true,
+  },
 })
